a28fe07993123964476ef7e4ec9d982fb7804d05
ashwin <ashwin@usistech.com>
2019-10-04 09:12:45 +0530

    Routing & Link image

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import Nav  from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
+import logo from './asserts/logo.png';
 
-class App extends Component {
-  user = {
-    name: "Ashwin"
-  };
+import Products from './components/Products';
+import PageNotFound from './components/PageNotFound';
 
-  render() {
-    return (
-     <h1>{user.name}</h1>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <Navbar bg="light" class='navbar'>
+        <Navbar.Brand>
+          <img src={logo} />
+        </Navbar.Brand>
+        <Nav>
+          <Nav.Link as={Link} to="/products">Products</Nav.Link>
+        </Nav>
+      </Navbar>
+      <Switch>
+        <Route path='/' exact component={ Products }/>
+        <Route path='/products' component={ Products }/>
+        <Route component={ PageNotFound }/>
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <>hi</>
+    )
+}
+export default Products;
